eb1907e21943891bedcf365cfba5e6f3ea8403f0
John Albert Flores <floresjohnalbert19@gmail.com>
2024-08-10 09:55:49 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple Todo App</title>
+</head>
+<body>
+  
+<h1>
+Simple ToDo App using PHP with proper (atleast) project structure
+</h1>
+
+<p>
+  This includes the basic CRUD operation for each task.
+</p>
+</body>
+</html>
